f3446257896b3a0d7809df29ddaec75708ccaa65
Fran Simó <fransimo@gmail.com>
2016-03-12 15:54:04 +0100

    tmp

--- tmp.py
+++ tmp.py
@@ -6,7 +6,7 @@ tn.write("auth " + user + " " + password + "\n")
 
 
 search = "IMG_0001.JPG"
-tn.write("s \"" + search + "\"\n")
+tn.write("s -minsize 1048576 \"" + search + "\"\n")
 s = tn.read_very_eager()
 tn.write("vr\n")
 s = tn.read_very_eager()
@@ -16,3 +16,11 @@ s3 = s.splitlines()
 s3[180].split()
 
 s3[189].split("urn:")
+
+
+Result of search 7
+218 results (-32 waiting)
+ [ Num ]     Size   Avail Status                       Names                          Tags                      MD4                    
+[     1] 22269088     1        IMG_0001.jpg                                                urn:ed2k:D36A3977BBD2D611115FA318BCA4D82C
+[     2] 20908075     1        IMG_0001.jpg                                       1        urn:ed2k:13BFDC79FF47980305E61D69022186E9
+[     3] 18316126     2        IMG_0001.jpg                                       1        urn:ed2k:9EC2CA819591FE46238B4DFB170B914A
