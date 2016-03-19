169c6235234727b9bc887d952bb04af22fd38aad
Fran Simó <fransimo@gmail.com>
2016-03-19 07:55:47 +0000

    sync bi

--- extra/elvex2disk.sh
+++ extra/elvex2disk.sh
@@ -5,7 +5,12 @@ rsync -avz  /datos/flickrGramas/jenkins/up_ok /datos/net/diskstationfsm/d4/Proye
 
 sudo bash ~/workspace/shared_folder/extra/e2k_photos.sh > log.txt
 
+#pasar las nuevas
 rsync -avz  /datos/media/mldonkey/in/1_FRAN_tech/emule_spy /datos/net/diskstationfsm/d4/Proyectos4/shared_folder
 
-sudo rm -fR /datos/media/mldonkey/in/1_FRAN_tech/emule_spy/*
+#borrar todo
+#sudo rm -fR /datos/media/mldonkey/in/1_FRAN_tech/emule_spy/*
+
+#borrar solo las que se han borrado en el nas
+rsync -avz --delete  /datos/net/diskstationfsm/d4/Proyectos4/shared_folder /datos/media/mldonkey/in/1_FRAN_tech/emule_spy
 
