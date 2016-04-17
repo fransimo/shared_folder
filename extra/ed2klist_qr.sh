8eced4ccfd7f2357b351b0722820ffb3c52b2dd3
Fran Simó <fran@MacBook-Air-de-Fran.local>
2016-04-17 12:09:34 +0200

    e2dk

---
+++ extra/ed2klist_qr.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
+
+for f in ` $* ` ; do
+
+	echo $f
+	ed2=` ed2k_hash $f `
+	echo $ed2
+	
+
+	file=$(basename "$f")
+	base=${file%%.*}
+	e=${file##*.}
+	e=${e:0:3}
+	dest=$dest/${base}.png
+
+	
+	echo Destino: $dest
+	qrencode -o $dest '${ed2}'	
+	echo ${ed2} >> $dest/ed2links.txt
+	
+	
+done
+IFS=$SAVEIFS
+
