a35dddb4cb384f66563b3cd58027676dddfdb00e
Fran Simó <fran@MacBook-Air-de-Fran.local>
2016-05-22 09:28:59 +0200

    borrar sync

--- extra/permisos_ml.sh
+++
@@ -1,9 +0,0 @@
-function permisos {
-	sudo chown -R mldonkey.mldonkey $1
-	sudo find $1  -type f -exec chmod 644 {} \;
-	sudo find $1  -type d -exec chmod 775 {} \;
-}
-
-permisos /datos/media/mldonkey/
-permisos /datos/media/archivo/coleccion/
-permisos /datos/compartido
